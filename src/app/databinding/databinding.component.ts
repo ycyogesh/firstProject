7b12b753f17a3923b797919131e7b8400900e110
ycyogesh <ycyogesh183@gmail.com>
2022-09-20 17:47:47 +0530

    two way binding and controller to view-kruthika

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatabindingComponent implements OnInit {
   userData:string='kruthika'
+  userInput:string=''
 
   constructor() { }
 
