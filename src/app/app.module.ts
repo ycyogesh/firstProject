980523acd06c7b72fb4ab01a98175d8413a45dcc
ycyogesh <ycyogesh183@gmail.com>
2022-09-20 17:42:08 +0530

    view to controller-kruthika

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.scss']
+})
+export class DatabindingComponent implements OnInit {
+  userData:string='kruthika'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  userdetail(event:any){
+    console.log(event.target.value)
+
+  }
+
+}
